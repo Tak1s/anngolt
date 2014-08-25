5e187562c9c81ffbf1b59f945c91cd127486376e
Tak1s <deedee604@gmail.com>
2014-08-25 22:39:48 +0300

    gix align menu

--- phpQuery/proxy.php
+++ phpQuery/proxy.php
@@ -1,11 +1,15 @@
 <?php 
-    //   if (isDomainAvailible('http://api-fotki.yandex.ru/api/users/annagoltsberg/albums/'))
+    //   if (isDomainAvailible('http://api-fotki.yandex.ru/api/users/annagoltsberg/?format=json'))
     require ('phpQuery.php');
     $url = $_GET['url'];
+
+    if ($url === "firstQuery"){
+    	$url = "http://api-fotki.yandex.ru/api/users/annagoltsberg/?format=json";
+    };
     
     $results_page = file_get_contents($url);
   
     $document = phpQuery::newDocument($results_page);
 
     echo $document;
-?>
+?> 
